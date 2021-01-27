659b91aeb8352aaa5f167f9eb5684fe6aa9065b3
DucQuan <quanhien1999@gmail.com>
2021-01-27 17:58:42 +0700

    Update function : add footer and header when has default example

---
+++ ConsoleApp1/ConfigClass.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class ConfigClass
+    {
+        public string folder_scan { get; set; }
+        public string filepath_default_header_footer { get; set; }
+
+        static public ConfigClass GetConfig()
+        {
+            ConfigClass conf;
+            StreamReader reader = new StreamReader(Environment.CurrentDirectory + "\\Config.json");
+            String str = reader.ReadToEnd();
+            reader.Close();
+            if (str == "" || str == null)
+            {
+                Console.WriteLine("Check Config.json for folder");
+                return null;
+            }
+            else
+            {
+                conf = JsonConvert.DeserializeObject<ConfigClass>(str);
+            }
+
+            return conf;
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,60 +1,55 @@
 ﻿using Spire.Doc;
 using Spire.Doc.Documents;
-using Spire.Doc.Interface;
 using System;
-using System.Collections.Generic;
 using System.IO;
+using Spire.Doc.Fields;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
 
 namespace ConsoleApp1
 {
     class Program
     {
+        static ConfigClass conf;
         static void Main(string[] args)
         {
-            ConfigClass conf = ConfigClass.GetConfig();
-            Console.WriteLine(conf.folder);
-            DirectoryInfo d = new DirectoryInfo(conf.folder);//Assuming Test is your Folder
-            /*//DirectoryInfo d = new DirectoryInfo(@"E:\New folder");//Assuming Test is your Folder
-            FileInfo[] Files = d.GetFiles("*.docx"); //Getting Text files
-
-            foreach (FileInfo file in Files)
+            try
             {
-                try
+                conf = ConfigClass.GetConfig();
+                if (conf == null)
                 {
-                    //ReadDocxWithSpire(file);
-
-                    string oldName = file.Name;
-                    int start = oldName.IndexOf("Bài");
-                    if (start == -1) continue;
-                    string newName = oldName.Substring(0, start) + "Câu" + oldName.Substring(start + 3);
-                    Console.WriteLine(newName);
-                    file.MoveTo(Path.Combine(file.Directory.FullName, newName));
+                    throw new Exception("Can not find folder scan.");
                 }
-                catch
-                {
-                    Console.WriteLine("Error: {0} ", file.Name);
-                    continue;
-                }
-            }*/
+                Console.WriteLine("Folder scan :" + conf.folder_scan);
+                DirectoryInfo d = new DirectoryInfo(conf.folder_scan);
 
-            //DirectoryInfo d = new DirectoryInfo(@"F:\Tools\ConsoleReadDocx");//Assuming Test is your Folder
-            try { 
+
                 FileInfo[] Files = d.GetFiles("*.docx"); //Getting Text files
+
+                int total_file = Files.Length;
+                int fix_file_number = 0;
+
                 foreach (FileInfo file in Files)
                 {
                     try
                     {
-                        RemoveWordInDocx(file, "quảng cáo");
+                        Console.WriteLine(file.Name);
+                        //RemoveWordInDocx(file, "quảng cáo");
+                        AddHeaderFooter(file);
+                        fix_file_number += 1;
                     }
                     catch (Exception ex)
                     {
                         Console.WriteLine("error in : " + file.Name);
-                        Console.WriteLine("error" + ex.Message);
+                        Console.WriteLine("error: " + ex.Message);
                         continue;
                     }
                 }
 
                 Console.WriteLine("Done");
+                Console.WriteLine("Total file : " + total_file);
+                Console.WriteLine("Fix file : " + fix_file_number);
                 Console.ReadLine();
             }
             catch
@@ -98,18 +93,18 @@ namespace ConsoleApp1
             Section section = document.Sections[0];
 
             int numberPara = section.Paragraphs.Count;
-            for(int i = 0; i < numberPara; i++)
+            for (int i = 0; i < numberPara; i++)
             {
                 Paragraph para = section.Paragraphs[i];
                 string oldString = para.Text;
                 int start = oldString.ToLower().IndexOf(word);
-                
+
                 if (start == -1)
                 {
                     continue;
                 }
                 else
-                {                 
+                {
                     string newString = oldString.Substring(0, start) + oldString.Substring(start + word.Length);
                     Console.WriteLine("Fixed in :" + file.Name);
                     Console.WriteLine("new: " + newString);
@@ -117,21 +112,65 @@ namespace ConsoleApp1
                     document.SaveToFile(file.FullName, FileFormat.Docx);
                 }
             }
-
-            //Update Text of Title
-            /*Section section = document.Sections[0];
-            Paragraph para1 = section.Paragraphs[0];
-            string oldString = para1.Text;
-            int start = oldString.IndexOf("Bài");
-            if (start == -1)
-            {
-                return;
-            }
-            string newString = oldString.Substring(0, start) + "Câu" + oldString.Substring(start + 3);
-            Console.WriteLine(newString);
-            para1.Text = newString;*/
-
-            //document.SaveToFile(file.FullName, FileFormat.Docx);
         }
+
+        public static HeadersFooters GetDefaultHeaderFooter(FileInfo file)
+        {
+            Document document = new Document();
+            document.LoadFromFile(file.FullName);
+
+            Section section = document.Sections[0];
+
+            HeadersFooters para_header_default = section.HeadersFooters;
+
+            return para_header_default;
+        }
+
+        public static void AddHeaderFooter(FileInfo file)
+        {
+            if (String.IsNullOrEmpty(conf.filepath_default_header_footer))
+            {
+                throw new Exception("Cant not find default headerfooter file");
+            }
+            //Console.WriteLine("Default file Header_Footer : " + conf.filepath_default_header_footer);
+
+            Document doc = new Document();
+            doc.LoadFromFile(file.FullName);
+
+            Section section = doc.Sections[0];
+
+            //remove header, footer from all page
+            section.HeadersFooters.Header.ChildObjects.Clear();
+            section.HeadersFooters.Footer.ChildObjects.Clear();
+
+
+            HeadersFooters default_headerfooter = GetDefaultHeaderFooter(new FileInfo(conf.filepath_default_header_footer));
+
+            //change header
+            HeaderFooter current_header = section.HeadersFooters.Header;
+
+            foreach (DocumentObject obj in default_headerfooter.Header.ChildObjects)
+            {
+                current_header.ChildObjects.Add(obj.Clone());
+            }
+
+            //change footer
+            HeaderFooter current_footer = section.HeadersFooters.Footer;
+
+            foreach (DocumentObject obj in default_headerfooter.Footer.ChildObjects)
+            {
+                current_footer.ChildObjects.Add(obj.Clone());
+            }
+
+            // change LeftIndent
+            current_header.Paragraphs[0].Format.LeftIndent = -72;
+            current_footer.Paragraphs[0].Format.LeftIndent = -71;
+
+            section.PageSetup.HeaderDistance = 0;
+            section.PageSetup.FooterDistance = 0;
+
+            doc.SaveToFile(file.FullName, FileFormat.Docx);
+        }
+
     }
 }
